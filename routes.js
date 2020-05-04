d221fa3ead1c3f5ce96d94d6525dba78611ee409
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-05-04 23:47:31 +0900

    Update routes.js

--- routes.js
+++ routes.js
@@ -10,8 +10,8 @@ const SEARCH = "/search";
 // Users
 const USERS = "/users";
 const USER_DETAIL = "/:id";
-const EDIT_PROFILE = "users/edit-profile";
-const CHANGE_PASSWORD = "users/change-password";
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
 
 // Videos
 const VIDEOS = "/videos";
