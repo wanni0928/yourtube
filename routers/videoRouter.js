5c61db768a6281aac5c38027663bc2dde32e7bc8
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-05-04 23:47:28 +0900

    Update videoRouter.js

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,16 @@
 import express from "express";
 import routes from "../routes"
-import { videos, upload, videDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import { videDetail, editVideo, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videDetail);
 videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 // videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videDetail);
 
 //export default 란 파일로 export 해준다는 뜻이다.
 export default videoRouter;
