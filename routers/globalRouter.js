28408ee6e3ac42d45014e6614d9437e397f11950
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-05-04 20:41:10 +0900

    Update globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,21 @@
 import express from "express";
 import routes from '../routes';
 import {home, search} from "../controllers/videoController";
-import {join, login, logout} from "../controllers/userController";
+import {getJoin, postJoin, logout, getLogin, postLogin} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+// globalRouter.get("/경로명", 익명의 함수.);
+// mvc 패턴으로 효율적으로 다루기 위해, controller js에 해당 url 주소와 그 주소에 전달할 view(이 경우엔 pug)를 담은 함수를 지정하고, import 해서, 상황에 따라 필요한 함수이름을 선언하였다.
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
