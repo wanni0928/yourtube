ea68267b06e78d923b8cdf94813dddd056b2fba6
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-19 18:21:17 +0900

    Update videoRouter.js

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,23 @@
 import express from "express";
 import routes from "../routes"
-import { videDetail, editVideo, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
+import { videDetail, getEditVideo, deleteVideo, getUpload, postUpload, postEditVideo } from "../controllers/videoController";
+import { uploadVideo, onlyPrivate } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+// Upload
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
+// Video Detail
 videoRouter.get(routes.videoDetail(), videDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 // videoRouter.get(routes.videos, videos);
 
 //export default 란 파일로 export 해준다는 뜻이다.
