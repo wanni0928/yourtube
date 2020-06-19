64290e17b3911df6ddac3e3f225d13a5b6b69889
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-19 18:21:15 +0900

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,22 @@
 import express from "express";
 import routes from "../routes"
-import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
+import { users, userDetail, getEditProfile, me, postEditProfile, getChangePassword, postChangePassword } from "../controllers/userController";
+import { onlyPrivate, uploadAvartar } from "../middlewares";
+import { postEditVideo } from "../controllers/videoController";
 
 const userRouter = express.Router();
 
 // userRouter.get(routes.users, users);
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail(), userDetail);
+
+// edit profile
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvartar, postEditProfile);
+// change password
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
+
+// user detail
+userRouter.get(routes.userDetail(), onlyPrivate, userDetail);
+// userRouter.get(routes.me, me);
 
 export default userRouter;
