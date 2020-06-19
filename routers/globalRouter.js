3ffc779f857bf6edd240fe1d54c14ee8c0b916c3
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-19 18:21:11 +0900

    Update globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,22 +1,42 @@
 import express from "express";
+import passport from "passport";
 import routes from '../routes';
 import {home, search} from "../controllers/videoController";
-import {getJoin, postJoin, logout, getLogin, postLogin} from "../controllers/userController";
+import {getJoin, postJoin, logout, getLogin, postLogin, githubLogin, postGithubLogIn, getMe, facebookLogin, postFacebookLogin} from "../controllers/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
 // globalRouter.get("/경로명", 익명의 함수.);
 // mvc 패턴으로 효율적으로 다루기 위해, controller js에 해당 url 주소와 그 주소에 전달할 view(이 경우엔 pug)를 담은 함수를 지정하고, import 해서, 상황에 따라 필요한 함수이름을 선언하였다.
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
-
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+//github login
+globalRouter.get(routes.github, githubLogin);
+globalRouter.get(
+    routes.githubCallback, 
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    postGithubLogIn
+);
+
+globalRouter.get(routes.me, getMe);
+
+//facebook login
+globalRouter.get(routes.facebook, facebookLogin);
+globalRouter.get(
+    routes.facebookCallback,
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    postFacebookLogin
+);
+
 
 export default globalRouter;
